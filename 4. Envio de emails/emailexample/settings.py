5383eba3f02ce3814061ddbdb385b3839a5006fc
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-07-13 22:42:21 -0500

    Arreglo credenciales Ejercicio 4. Listo para grabar

--- 4. Envio de emails/emailexample/settings.py
+++ 4. Envio de emails/emailexample/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rzapata@unsa.edu.pe'
-EMAIL_HOST_PASSWORD = 'zapatareyser1'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
